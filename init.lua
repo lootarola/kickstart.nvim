a5fe46082defcaaca34314df01e9fdbc03419c49
lootarola <lucas.otarola@gmail.com>
2025-02-18 00:19:57 -0300

    🐛 Disable html files highlighting on treesitter
    Theres a bug with html files and highlighting on treesitter while using telescope on nvim 0.10

--- init.lua
+++ init.lua
@@ -945,12 +945,14 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    branch = 'master',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'html' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
